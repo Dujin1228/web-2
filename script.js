d69c102e3b585dfe128bab99ec5883c784c6aea0
Dujin1228 <49527407+Dujin1228@users.noreply.github.com>
2019-04-16 06:20:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ $(document).ready(function() {
 
     if (class_by_id == 'touch on') {
       document.getElementById("touch-button").className="touch off";
+      document.getElementById("menu").className="menu off";
+      document.getElementById("opacity").className="opacity menu-off"
     }
     else {
       document.getElementById("touch-button").className="touch on"
+      document.getElementById("menu").className="menu on";
+      document.getElementById("opacity").className="opacity menu-on"
     }
   });
 
 });
-$(this).toggleClass('on');
